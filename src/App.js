c6c7efdd9dc551c58e3289ab27f7a2f9e3cffeb6
SREELEKSHMISABU <79195693+SREELEKSHMISABU@users.noreply.github.com>
2025-06-15 12:18:29 +0530

    Update App.js
    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // ← changed
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
 import EmployeeForm from './components/EmployeeForm';
